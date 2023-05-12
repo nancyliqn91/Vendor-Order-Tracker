97b6ba09e91974c392dcb3d25ba77014f7ed4713
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-12 09:24:31 -0700

    add test 3

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -9,15 +9,37 @@ namespace VendorOrderTracker.Tests
   {
     public void Dispose()
     {
-      Order.ClearAll();
+      // Order.ClearAll();
     }
 
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Title","bread", 40, 10);
+      Order newOrder = new Order("title","bread", 40, 10);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      Order newOrder1 = new Order("title","bread", 40, 10);
+      Order newOrder2 = new Order("title","pastry", 45, 9);
+      List<Order> newOrder = new List<Order> { newOrder1, newOrder2 };
+      //Act
+      List<Order> result = Order.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newOrder, result);
+    }
+
+    // [TestMethod]
+    // public void ClearAll_ClearInstanceOfOrder_()
+    // {
+    //   Order newOrder = new Order("title","bread", 40, 10);
+    //   List<Order> expected = new List<Order> {};
+    //   Order.ClearAll();
+    //   CollectionAssert.AreEqual(expected, newOrder.ClearAll());
+    // }
 
 
   }
